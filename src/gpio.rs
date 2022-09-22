765025f346e18ec1fc827a57851f05f75c8aa106
Sheref Sidarous <sherefyounan@gmail.com>
2022-09-21 22:43:20 -0700

    hooked up the interrupt and wrote the poll logic, but the waker is not connected yet

--- src/gpio.rs
+++ src/gpio.rs
@@ -2,14 +2,23 @@
 use esp32c3_hal::{
     gpio::{Gpio9, IO},
     gpio_types::{Event, Input, PullDown},
+    interrupt,
 };
+use esp32c3_hal::prelude::interrupt;
+use esp32c3_hal::pac::{self};
+
 
 use core::future::Future;
 use core::task::{Context, Poll, Waker};
 use core::pin::Pin;
+use core::cell::RefCell;
+
+use esp32c3_hal::gpio_types::Pin as _;
+
+use critical_section::Mutex;
 
 pub struct EmbGPIO<F> {
-    hal_gpio : Gpio9<Input<PullDown>>,
+    //hal_gpio : Gpio9<Input<PullDown>>,
     toggled_future : F
 }
 
@@ -31,22 +40,40 @@ impl ToggledFuture {
 
 impl Copy for ToggledFuture {}
 
+static recently_toggled : Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
+static BUTTON: Mutex<RefCell<Option<Gpio9<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
+
 impl Future for ToggledFuture {
     type Output = ();
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-    
-            //Poll::Pending
 
-            Poll::Ready(())
+        let mut outcome = Poll::Pending;
+
+        critical_section::with(|cs| {
+            let mut recently_toggled_ref = recently_toggled.borrow_ref_mut(cs);
+            if *recently_toggled_ref == true {
+                *recently_toggled_ref = false;
+                outcome = Poll::Ready(());
+            }
+        });
+
+        return outcome;
 
     }
 }
 
 impl EmbGPIO<ToggledFuture> {
 
-    pub fn new<T>(gpio : Gpio9<T>) -> Self { 
+    pub fn new<T>(gpio : Gpio9<T>) -> Self {
+
+        let mut button = gpio.into_pull_down_input();
+        button.listen(Event::FallingEdge);
+    
+        critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(button));
+
+        interrupt::enable(pac::Interrupt::GPIO, interrupt::Priority::Priority3).unwrap();
+
         EmbGPIO {
-            hal_gpio: gpio.into_pull_down_input(),            
             toggled_future : ToggledFuture::new(),
         }
     }
@@ -58,3 +85,17 @@ impl EmbGPIO<ToggledFuture> {
 
 
 
+#[interrupt]
+fn GPIO() {
+    critical_section::with(|cs| {
+        BUTTON
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .clear_interrupt();
+
+        *recently_toggled.borrow_ref_mut(cs) = true;
+    });
+
+    // TODO: call the waker
+}
